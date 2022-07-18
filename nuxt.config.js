e8de1fdd1928274465dc553d01c81fdd2612cb1e
Yisrae <yisrae@fedorae.com>
2022-07-18 02:14:16 +0100

    Added comment notes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,10 @@
 export default {
-  ssr: false,
+  ssr: false, // Remove to set rendering on server-side
+  /*
+   * Note: Icons will load after the page is rendered if set to server-side!
+   */
 
-  target: "static",
+  target: "static", // Remove to set target as 'server'
 
   head: {
     titleTemplate: "nuxt-uikit",
